4798cb2b80f88fc67db22af3ec4cea4330bd0a5f
aria <aryashahbazi@gmail.com>
2021-04-03 16:06:38 +0430

    Data structure
    Bubble Sort

---
+++ Build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: G:/C++/AW/Bubble_Sort/Bubble_Sort
+# Build directory: G:/C++/AW/Bubble_Sort/Bubble_Sort/Build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
---
+++ Build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: G:/C++/AW/Bubble_Sort/Bubble_Sort/tests
+# Build directory: G:/C++/AW/Bubble_Sort/Bubble_Sort/Build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(myLibrary.unit_tests "G:/C++/AW/Bubble_Sort/Bubble_Sort/Build/unit_tests.exe")
+set_tests_properties(myLibrary.unit_tests PROPERTIES  _BACKTRACE_TRIPLES "G:/C++/AW/Bubble_Sort/Bubble_Sort/tests/CMakeLists.txt;22;add_test;G:/C++/AW/Bubble_Sort/Bubble_Sort/tests/CMakeLists.txt;0;")
